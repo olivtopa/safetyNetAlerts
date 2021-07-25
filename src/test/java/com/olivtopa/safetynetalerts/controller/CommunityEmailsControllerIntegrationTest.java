9216f78df1b620ad33c5a954140c5febbbdca8fb
Olivier <amoliv_825@hotmail.com>
2021-07-25 17:48:37 +0200

    creation de PhoneAlertControllerIntegrationTest

--- src/test/java/com/olivtopa/safetynetalerts/controller/CommunityEmailsControllerIntegrationTest.java
+++ src/test/java/com/olivtopa/safetynetalerts/controller/CommunityEmailsControllerIntegrationTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CommunityEmailsControllerIT {
+class CommunityEmailsControllerIntegrationTest {
 
 	public static final String CITY_NAME = "Culver";
 	@Autowired
--- src/test/java/com/olivtopa/safetynetalerts/controller/PersonControllerIT.java
+++
@@ -1,5 +0,0 @@
-package com.olivtopa.safetynetalerts.controller;
-
-public class PersonControllerIT {
-
-}
