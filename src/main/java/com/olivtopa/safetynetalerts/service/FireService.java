09cae9c2dacb2156029d2ff856b6f06a2f9c646e
Olivier <amoliv_825@hotmail.com>
2021-07-27 16:51:14 +0200

    modification FireService

--- src/main/java/com/olivtopa/safetynetalerts/service/FireService.java
+++ src/main/java/com/olivtopa/safetynetalerts/service/FireService.java
@@ -2,6 +2,7 @@ package com.olivtopa.safetynetalerts.service;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,25 +17,35 @@ import com.olivtopa.safetynetalerts.model.FiresStation;
 
 @Service
 public class FireService {
-	
+
 	@Autowired
 	private MedicalRecordDAO medicalRecordDAO;
 	@Autowired
 	private FireStationDAO fireStationDAO;
 	@Autowired
 	private PersonDAO personDAO;
-	
-	public List<Fire> findByAddress(String address) {
-		List<String> personPhone = personDAO.getAll().stream().filter(person -> person.getAddress().equals(address))
-				.map(Person::getPhone).collect(Collectors.toList());
-		List<String> personLastName = personDAO.getAll().stream().filter(person -> person.getAddress().equals(address))
-				.map(Person::getLastName).collect(Collectors.toList());
-		List<Integer> station = fireStationDAO.getAll().stream().filter(firesStation -> firesStation.getAddress().equals(address))
-				.map(FiresStation::getStation).collect(Collectors.toList());
-		List<List> medical = medicalRecordDAO.getAll().stream().filter(medic -> medic.getLastName().equals(address))
-				.map(MedicalRecord::getAllergies).collect(Collectors.toList());
+
+	private String personLastName;
+	private String personPhone;
+	private int station;
+	private List<String> allergies;
+
+	private Fire buildFire(Person person, FiresStation firesStation, MedicalRecord medicalRecord) {
+
+		Fire fire = new Fire();
+
+		person.setLastName(personLastName);
+		person.setPhone(personPhone);
+		firesStation.setStation(station);
+		medicalRecord.setAllergies(allergies);
+
+		return fire;
+
+	}
+
+	private FiresStation findFiresStation(String address) {
 		
-		return null;
-		
+		FiresStation findFireStation = fireStationDAO.getAll().stream().filter(a->a.getAddress().equals(address)).map(FiresStation::getStation);
+		return findFireStation;
 	}
 }
