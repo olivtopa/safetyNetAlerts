25c88d64c0e1651a7815d6d3b3ed9e6b23a36595
Olivier <amoliv_825@hotmail.com>
2021-09-06 22:11:36 +0200

    Creation filtre par num station, Test u

--- src/main/java/com/olivtopa/safetynetalerts/service/FirestationService.java
+++ src/main/java/com/olivtopa/safetynetalerts/service/FirestationService.java
@@ -2,10 +2,13 @@ package com.olivtopa.safetynetalerts.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.olivtopa.safetynetalerts.dao.PersonDAO;
+import com.olivtopa.safetynetalerts.dao.FireStationDAO;
+import com.olivtopa.safetynetalerts.model.FiresStation;
 import com.olivtopa.safetynetalerts.model.Person;
 import com.olivtopa.safetynetalerts.model.PersonInFireStation;
 import com.olivtopa.safetynetalerts.model.PersonsInFireStation;
@@ -14,17 +17,28 @@ import com.olivtopa.safetynetalerts.model.PersonsInFireStation;
 public class FirestationService {
 	
 	private final PersonDAO personDAO;
+	private final FireStationDAO fireStationDAO;
 	
-	public FirestationService(PersonDAO personDAO) {
+	public FirestationService(PersonDAO personDAO, FireStationDAO fireStationDAO) {
 		this.personDAO = personDAO;
+		this.fireStationDAO = fireStationDAO;
 	}
 
-	public PersonsInFireStation findPersonsInFireStationScope(long stationNumber) {
-		
-		List<Person> allPersons = personDAO.getAll();
-		
-		return buildResult(allPersons);
-	}
+
+
+public PersonsInFireStation findPersonsInFireStationScope(long stationNumber) {
+
+    FiresStation fireStation = fireStationDAO.getAll().stream()
+            .filter(firesStation -> firesStation.getStation() == stationNumber)
+            .findFirst().orElseThrow();
+
+    List<Person> allPersons = personDAO.getAll().stream().filter(person -> person.getAddress().equals(fireStation.getAddress()))
+            .collect(Collectors.toList());
+
+    return buildResult(allPersons);
+}
+
+
 
 	private PersonsInFireStation buildResult(List<Person> allPersons) {
 		PersonsInFireStation personsInFireStation = new PersonsInFireStation();
--- src/test/java/com/olivtopa/safetynetalerts/service/FirestationServiceTest.java
+++ src/test/java/com/olivtopa/safetynetalerts/service/FirestationServiceTest.java
@@ -11,7 +11,9 @@ import static org.mockito.Mockito.when;
 
 import java.util.List;
 
+import com.olivtopa.safetynetalerts.dao.FireStationDAO;
 import com.olivtopa.safetynetalerts.dao.PersonDAO;
+import com.olivtopa.safetynetalerts.model.FiresStation;
 import com.olivtopa.safetynetalerts.model.Person;
 import com.olivtopa.safetynetalerts.model.PersonsInFireStation;
 
@@ -23,16 +25,30 @@ class FirestationServiceTest {
 
 	@Mock
 	private PersonDAO personDAO;
+	
+	@Mock
+    private FireStationDAO fireStationDAO;
 
 	@Test
 	void findPersonInFireStationScope() {
 
 		// GIVEN
 		Person person1 = new Person();
-		person1.setFirstName("Léo"); // Cette valeur doit se retrouver dans le résultat
-		person1.setLastName("Durand"); // Celle-ci également
+		person1.setFirstName("Léo"); 
+		person1.setLastName("Durand");
 		person1.setAddress("1509 Culver St");
-		when(personDAO.getAll()).thenReturn(List.of(person1));
+
+		Person outOfFireStationPerson = new Person();
+		outOfFireStationPerson.setFirstName("Thibaut");
+		outOfFireStationPerson.setLastName("Donovan");
+		outOfFireStationPerson.setAddress("892 Downing Ct");
+		
+		FiresStation fireStation = new FiresStation();
+	    fireStation.setStation(1);
+	    fireStation.setAddress("1509 Culver St");
+	    when(fireStationDAO.getAll()).thenReturn(List.of(fireStation));
+
+		when(personDAO.getAll()).thenReturn(List.of(person1, outOfFireStationPerson));
 
 		// When
 		PersonsInFireStation resultat = firestationService.findPersonsInFireStationScope(1L);
