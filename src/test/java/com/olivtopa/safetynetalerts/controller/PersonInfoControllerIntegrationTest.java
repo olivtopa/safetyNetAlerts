94aaa16ba5351ebfc3bc63053e511b047feb1c05
Olivier <amoliv_825@hotmail.com>
2021-09-08 14:37:21 +0200

    modification du type de personInfo en objet, puis modification de l assertion

--- src/test/java/com/olivtopa/safetynetalerts/controller/PersonInfoControllerIntegrationTest.java
+++ src/test/java/com/olivtopa/safetynetalerts/controller/PersonInfoControllerIntegrationTest.java
@@ -1,7 +1,5 @@
 package com.olivtopa.safetynetalerts.controller;
 
-import java.util.List;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,22 +8,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.olivtopa.safetynetalerts.model.PersonInfo;
 
 @SpringBootTest
-public class PersonInfoServiceControllerIntegrationTest {
-	
+public class PersonInfoControllerIntegrationTest {
+
 	public static String FIRSTNAME = "John";
 	public static String LASTNAME = "Boyd";
-	
+
 	@Autowired
 	private PersonInfoController controller;
-	
+
 	@Test
 	public void inhabitantByName() {
-		
+
 		// GIVEN + WHEN
-		List<PersonInfo> personInfo = controller.getPersonInfo(FIRSTNAME, LASTNAME);
-		
-		//THEN
-		Assertions.assertThat(personInfo).isNotEmpty();
+		PersonInfo personInfo = controller.returnPersonInfo(FIRSTNAME, LASTNAME);
+
+		// THEN
+		Assertions.assertThat(personInfo).isNotNull();
 	}
 
 }
