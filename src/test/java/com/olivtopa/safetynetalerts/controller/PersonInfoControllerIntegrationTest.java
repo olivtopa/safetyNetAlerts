2eb35275c084da93be4e8d6465b2eec0a8f2da8f
Olivier <amoliv_825@hotmail.com>
2021-09-08 20:01:26 +0200

    pas de modif

--- src/test/java/com/olivtopa/safetynetalerts/controller/PersonInfoControllerIntegrationTest.java
+++ src/test/java/com/olivtopa/safetynetalerts/controller/PersonInfoControllerIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.olivtopa.safetynetalerts.controller;
 
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +22,7 @@ public class PersonInfoControllerIntegrationTest {
 	public void inhabitantByName() {
 
 		// GIVEN + WHEN
-		PersonInfo personInfo = controller.returnPersonInfo(FIRSTNAME, LASTNAME);
+		List<PersonInfo>  personInfo = controller.returnPersonInfo(FIRSTNAME, LASTNAME);
 
 		// THEN
 		Assertions.assertThat(personInfo).isNotNull();
