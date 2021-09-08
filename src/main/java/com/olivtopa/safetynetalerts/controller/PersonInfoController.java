0c64ae7e46a1bbf89cbd6a5943d3b8996dffcb81
Olivier <amoliv_825@hotmail.com>
2021-09-08 19:44:22 +0200

    maj

--- src/main/java/com/olivtopa/safetynetalerts/controller/PersonInfoController.java
+++ src/main/java/com/olivtopa/safetynetalerts/controller/PersonInfoController.java
@@ -1,5 +1,10 @@
 package com.olivtopa.safetynetalerts.controller;
 
+
+
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,13 +15,13 @@ import com.olivtopa.safetynetalerts.service.PersonInfoService;
 
 @RestController
 public class PersonInfoController {
-
+	
 	@Autowired
 	PersonInfoService personInfoService;
-
+	
 	@RequestMapping(value = "/personInfo", method = RequestMethod.GET)
-	public PersonInfo returnPersonInfo(String FirstName, String LastName) {
-
+	public List<PersonInfo> returnPersonInfo(String FirstName, String LastName){
+		
 		return personInfoService.personDetails(FirstName, LastName);
 	}
 
