b469d34d9f704216f13eeab4d2be95783638f5a5
Olivier <amoliv_825@hotmail.com>
2021-09-08 19:01:31 +0200

    correction prise en compte format localDate

--- src/main/java/com/olivtopa/safetynetalerts/model/MedicalRecord.java
+++ src/main/java/com/olivtopa/safetynetalerts/model/MedicalRecord.java
@@ -3,10 +3,13 @@ package com.olivtopa.safetynetalerts.model;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class MedicalRecord {
 
 	private String firstName;
 	private String lastName;
+	@JsonFormat(pattern = "MM/dd/yyyy")
 	private LocalDate birthdate;
 	private List<String> medications;
 	private List<String> allergies;
