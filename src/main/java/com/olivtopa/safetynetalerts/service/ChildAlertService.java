ca5193849d406dc7d15a00faad29f2a2ad8b6a3e
Olivier <amoliv_825@hotmail.com>
2021-09-10 00:22:23 +0200

    service fonctionnel, tu non fonctionnel

--- src/main/java/com/olivtopa/safetynetalerts/service/ChildAlertService.java
+++ src/main/java/com/olivtopa/safetynetalerts/service/ChildAlertService.java
@@ -1,13 +1,64 @@
 package com.olivtopa.safetynetalerts.service;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.olivtopa.safetynetalerts.model.ChildrenList;
+
+import com.olivtopa.safetynetalerts.dao.MedicalRecordDAO;
+import com.olivtopa.safetynetalerts.dao.PersonDAO;
+import com.olivtopa.safetynetalerts.model.PersonList;
+
+import com.olivtopa.safetynetalerts.model.MedicalRecord;
+import com.olivtopa.safetynetalerts.model.Person;
 
 @Service
 public class ChildAlertService {
 
-	public Object finalChildrenList(String address) {
-		return new ChildrenList();
+	@Autowired
+	private PersonDAO personDAO;
+	
+	@Autowired
+	private MedicalRecordDAO medicalRecordDAO;
+	
+	
+	private PersonList buildChildrenList(Person person, MedicalRecord medicalRecord) {
+
+		PersonList childrenList = new PersonList();
+
+		childrenList.setFirstName(person.getFirstName());
+		childrenList.setLastName(person.getLastName());
+
+		int age;
+		LocalDate birthdate = medicalRecord.getBirthdate();
+		LocalDate currentdate = LocalDate.now();
+
+		age = calculateAge(birthdate, currentdate);
+
+		childrenList.setAge(age);
+
+		return childrenList;
+
 	}
 
+	public List<PersonList> finalChildrenList(String address) {
+		List<PersonList> foyer = personDAO.getAll().stream().filter(a -> a.getAddress().equals(address)).map(
+				person -> buildChildrenList(person, findMedicalRecord(person.getFirstName(), person.getLastName())))
+				.collect(Collectors.toList());
+		return foyer;
+	}
+
+	private MedicalRecord findMedicalRecord(String firstName, String lastName) {
+		MedicalRecord medical = medicalRecordDAO.getAll().stream()
+				.filter(person -> person.getFirstName().equals(firstName) && person.getLastName().equals(lastName))
+				.distinct().findAny().orElse(null);
+		return medical;
+	}
+
+	public int calculateAge(LocalDate birthdate, LocalDate currentDate) {
+		return Period.between(birthdate, currentDate).getYears();
+	}
 }
--- src/test/java/com/olivtopa/safetynetalerts/service/ChildAlertServiceTest.java
+++ src/test/java/com/olivtopa/safetynetalerts/service/ChildAlertServiceTest.java
@@ -1,23 +1,51 @@
 package com.olivtopa.safetynetalerts.service;
 
-import org.junit.jupiter.api.Assertions;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.olivtopa.safetynetalerts.dao.MedicalRecordDAO;
+import com.olivtopa.safetynetalerts.dao.PersonDAO;
+import com.olivtopa.safetynetalerts.model.PersonList;
+import com.olivtopa.safetynetalerts.model.MedicalRecord;
+import com.olivtopa.safetynetalerts.model.Person;
 
 @ExtendWith(MockitoExtension.class)
 public class ChildAlertServiceTest {
-	
+
 	@InjectMocks
 	private ChildAlertService childAlertService;
-	
+
+	@Mock
+	PersonDAO personDAO;
+	@Mock
+	MedicalRecordDAO medicalRecordDAO;
+
 	@Test
 	void finalChildrenList() {
-		
-		Object resultat = childAlertService.finalChildrenList("1509 Culver St");
-		
-		Assertions.assertNotNull(resultat);
-	}
 
+		// GIVEN
+		Person person1 = new Person();
+		person1.setAddress("address1");
+		person1.setFirstName("Oliv");
+
+		MedicalRecord medicalRecord1 = new MedicalRecord();
+		medicalRecord1.setFirstName("Oliv");
+		medicalRecord1.setLastName("DUPONT");
+
+		Mockito.when(personDAO.getAll()).thenReturn(List.of(person1));
+		Mockito.when(medicalRecordDAO.getAll()).thenReturn(List.of(medicalRecord1));
+		// WHEN
+		List<PersonList> result = childAlertService.finalChildrenList("address1");
+
+		// THEN
+		Assertions.assertThat(result.contains(person1));
+	}
 }
