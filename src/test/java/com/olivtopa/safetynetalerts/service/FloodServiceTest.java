04acddc3ba28cc2f6800e984adbd23bde5c35a1b
Olivier <amoliv_825@hotmail.com>
2021-09-15 14:35:59 +0200

    maj

--- src/test/java/com/olivtopa/safetynetalerts/service/FloodServiceTest.java
+++ src/test/java/com/olivtopa/safetynetalerts/service/FloodServiceTest.java
@@ -20,51 +20,50 @@ import com.olivtopa.safetynetalerts.model.FloodFoyer;
 
 @ExtendWith(MockitoExtension.class)
 public class FloodServiceTest {
-	 @InjectMocks
-	 private FloodService floodService;
-	 
-	 @Mock
-		PersonDAO personDAO;
-		@Mock
-		MedicalRecordDAO medicalRecordDAO;
+	@InjectMocks
+	private FloodService floodService;
 
-		@Test
-		public void adressReturnFoyer() {
-			
-			// GIVEN
-			Person person = new Person();
-			person.setFirstName("Oliv");
-			person.setLastName("DUPONT");
-			person.setAddress("address1");
-			person.setPhone("11 22 33 44 55");
+	@Mock
+	PersonDAO personDAO;
+	@Mock
+	MedicalRecordDAO medicalRecordDAO;
 
-			MedicalRecord medicalRecord1 = new MedicalRecord();
-			medicalRecord1.setFirstName("Oliv");
-			medicalRecord1.setLastName("DUPONT");
-			medicalRecord1.setBirthdate(LocalDate.of(2000, 5, 17));
-			
-			Person person2 = new Person();
-			person2.setFirstName("Gil");
-			person2.setLastName("DUPONT");
-			person2.setAddress("address1");
-			person2.setPhone("22 33 44 55 66");
-			
-			MedicalRecord medicalRecord2 = new MedicalRecord();
-			medicalRecord2.setFirstName("Gil");
-			medicalRecord2.setLastName("DUPONT");
-			medicalRecord2.setBirthdate(LocalDate.of(2010, 8, 20));
+	@Test
+	public void adressReturnFoyer() {
 
-			Mockito.when(personDAO.getAll()).thenReturn(List.of(person, person2));
-			Mockito.when(medicalRecordDAO.getAll()).thenReturn(List.of(medicalRecord1,medicalRecord2));
-			// WHEN
-			List<FloodFoyer> result = floodService.floodPersonListByAddress("address1");
+		// GIVEN
+		Person person = new Person();
+		person.setFirstName("Oliv");
+		person.setLastName("DUPONT");
+		person.setAddress("address1");
+		person.setPhone("11 22 33 44 55");
 
-			// THEN
-			Assertions.assertThat(result).extracting(FloodFoyer::getFirstName, FloodFoyer::getLastName, FloodFoyer::getPhone)
-					.contains(new Tuple("Oliv", "DUPONT","11 22 33 44 55" ), new Tuple("Gil","DUPONT","22 33 44 55 66"));
-			
-		}
-	
-	
+		MedicalRecord medicalRecord1 = new MedicalRecord();
+		medicalRecord1.setFirstName("Oliv");
+		medicalRecord1.setLastName("DUPONT");
+		medicalRecord1.setBirthdate(LocalDate.of(2000, 5, 17));
+
+		Person person2 = new Person();
+		person2.setFirstName("Gil");
+		person2.setLastName("DUPONT");
+		person2.setAddress("address1");
+		person2.setPhone("22 33 44 55 66");
+
+		MedicalRecord medicalRecord2 = new MedicalRecord();
+		medicalRecord2.setFirstName("Gil");
+		medicalRecord2.setLastName("DUPONT");
+		medicalRecord2.setBirthdate(LocalDate.of(2010, 8, 20));
+
+		Mockito.when(personDAO.getAll()).thenReturn(List.of(person, person2));
+		Mockito.when(medicalRecordDAO.getAll()).thenReturn(List.of(medicalRecord1, medicalRecord2));
+		// WHEN
+		List<FloodFoyer> result = floodService.floodPersonListByAddress("address1");
+
+		// THEN
+		Assertions.assertThat(result)
+				.extracting(FloodFoyer::getFirstName, FloodFoyer::getLastName, FloodFoyer::getPhone)
+				.contains(new Tuple("Oliv", "DUPONT", "11 22 33 44 55"), new Tuple("Gil", "DUPONT", "22 33 44 55 66"));
+
+	}
 
 }
