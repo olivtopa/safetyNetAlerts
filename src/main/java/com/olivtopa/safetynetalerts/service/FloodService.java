d6589548a7f15735fb1919b22b06d72c66c459bc
Olivier <amoliv_825@hotmail.com>
2021-09-15 13:29:22 +0200

    creation service et test de creation d un foyer

--- src/main/java/com/olivtopa/safetynetalerts/service/FloodService.java
+++ src/main/java/com/olivtopa/safetynetalerts/service/FloodService.java
@@ -1,15 +1,67 @@
 package com.olivtopa.safetynetalerts.service;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.olivtopa.safetynetalerts.model.PersonsInFirestationList;
+import com.olivtopa.safetynetalerts.dao.MedicalRecordDAO;
+import com.olivtopa.safetynetalerts.dao.PersonDAO;
+import com.olivtopa.safetynetalerts.model.FloodFoyer;
+import com.olivtopa.safetynetalerts.model.MedicalRecord;
+import com.olivtopa.safetynetalerts.model.Person;
 
 @Service
 public class FloodService {
-	
-	public Object findPersonsInFireStationsList(long firestationNumber1,long firestationNumber2,
-			long firestationNumber3,long firestationNumber4) {
-		return new PersonsInFirestationList();
+
+	@Autowired
+	private PersonDAO personDAO;
+
+	@Autowired
+	private MedicalRecordDAO medicalRecordDAO;
+
+	private FloodFoyer buildFloodFoyer(Person person, MedicalRecord medicalRecord) {
+
+		FloodFoyer floodFoyer = new FloodFoyer();
+
+		floodFoyer.setFirstName(person.getFirstName());
+		floodFoyer.setLastName(person.getLastName());
+		floodFoyer.setAddress(person.getAddress());
+		floodFoyer.setPhone(person.getPhone());
+		floodFoyer.setMedications(medicalRecord.getMedications());
+		floodFoyer.setAllergies(medicalRecord.getAllergies());
+
+		int age;
+		LocalDate birthdate = medicalRecord.getBirthdate();
+		LocalDate currentdate = LocalDate.now();
+
+		age = calculateAge(birthdate, currentdate);
+
+		floodFoyer.setAge(age);
+
+		return floodFoyer;
+
+	}
+
+	public List<FloodFoyer> floodPersonListByAddress(String address) {
+		List<FloodFoyer> foyer = personDAO.getAll().stream().filter(a -> a.getAddress().equals(address)).map(
+				person -> buildFloodFoyer(person, findMedicalRecord(person.getFirstName(), person.getLastName())))
+				.collect(Collectors.toList());
+		return foyer;
+	}
+
+	private MedicalRecord findMedicalRecord(String firstName, String lastName) {
+		MedicalRecord medical = medicalRecordDAO.getAll().stream()
+				.filter(person -> person.getFirstName().equals(firstName) && person.getLastName().equals(lastName))
+				.distinct().findAny().orElseThrow();
+		return medical;
+	}
+
+	public int calculateAge(LocalDate birthdate, LocalDate currentDate) {
+		return Period.between(birthdate, currentDate).getYears();
 	}
 
 }
--- src/test/java/com/olivtopa/safetynetalerts/service/FloodServiceTest.java
+++ src/test/java/com/olivtopa/safetynetalerts/service/FloodServiceTest.java
@@ -1,23 +1,70 @@
 package com.olivtopa.safetynetalerts.service;
 
-import org.junit.jupiter.api.Assertions;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.olivtopa.safetynetalerts.dao.MedicalRecordDAO;
+import com.olivtopa.safetynetalerts.dao.PersonDAO;
+import com.olivtopa.safetynetalerts.model.MedicalRecord;
+import com.olivtopa.safetynetalerts.model.Person;
+import com.olivtopa.safetynetalerts.model.FloodFoyer;
 
 @ExtendWith(MockitoExtension.class)
 public class FloodServiceTest {
+	 @InjectMocks
+	 private FloodService floodService;
+	 
+	 @Mock
+		PersonDAO personDAO;
+		@Mock
+		MedicalRecordDAO medicalRecordDAO;
+
+		@Test
+		public void adressReturnFoyer() {
+			
+			// GIVEN
+			Person person = new Person();
+			person.setFirstName("Oliv");
+			person.setLastName("DUPONT");
+			person.setAddress("address1");
+			person.setPhone("11 22 33 44 55");
+
+			MedicalRecord medicalRecord1 = new MedicalRecord();
+			medicalRecord1.setFirstName("Oliv");
+			medicalRecord1.setLastName("DUPONT");
+			medicalRecord1.setBirthdate(LocalDate.of(2000, 5, 17));
+			
+			Person person2 = new Person();
+			person2.setFirstName("Gil");
+			person2.setLastName("DUPONT");
+			person2.setAddress("address1");
+			person2.setPhone("22 33 44 55 66");
+			
+			MedicalRecord medicalRecord2 = new MedicalRecord();
+			medicalRecord2.setFirstName("Gil");
+			medicalRecord2.setLastName("DUPONT");
+			medicalRecord2.setBirthdate(LocalDate.of(2010, 8, 20));
+
+			Mockito.when(personDAO.getAll()).thenReturn(List.of(person, person2));
+			Mockito.when(medicalRecordDAO.getAll()).thenReturn(List.of(medicalRecord1,medicalRecord2));
+			// WHEN
+			List<FloodFoyer> result = floodService.floodPersonListByAddress("address1");
+
+			// THEN
+			Assertions.assertThat(result).extracting(FloodFoyer::getFirstName, FloodFoyer::getLastName, FloodFoyer::getPhone)
+					.contains(new Tuple("Oliv", "DUPONT","11 22 33 44 55" ), new Tuple("Gil","DUPONT","22 33 44 55 66"));
+			
+		}
 	
-	@InjectMocks
-	private FloodService floodService;
 	
-	@Test
-	void findPersonsInFireStationList() {
-		
-		Object resultat = floodService.findPersonsInFireStationsList(1, 2, 3, 4);
-		
-		Assertions.assertNotNull(resultat);
-	}
 
 }
